142fe8f521908024a5af70aa7fcf3d1b5aead869
junochoi <hacke1123@naver.com>
2016-10-06 16:44:44 +0900

    no message

---
+++ Assets/___Scripts/---Ingame/objs/03Enemys/NewPlantCollider.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewPlantCollider : MonoBehaviour {
+	public GameObject Camera_ingame;
+	public Animator plant_ani;
+	public GameObject plantDeadPoint;
+	public GameObject plantModel;
+	public GameObject plantModel_dead;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerExit(Collider player){
+		if (player.CompareTag ("player")) {
+			Camera_ingame = player.GetComponent<PlayerMove> ().Camera_ingame;
+			Camera_ingame.GetComponent<GameCamera> ().viveCheck = true;
+			plant ();
+		}
+	}
+
+	void plant(){
+
+	}
+}
