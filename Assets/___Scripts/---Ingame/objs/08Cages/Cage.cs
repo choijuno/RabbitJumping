f68c3270850625ac2e806627a78630c59c2d9995
junochoi_PC <hacke11233@gmail.com>
2016-10-14 17:51:40 +0900

    no message

--- Assets/___Scripts/---Ingame/objs/08Cages/Cage.cs
+++ Assets/___Scripts/---Ingame/objs/08Cages/Cage.cs
@@ -8,24 +8,25 @@ public class Cage : MonoBehaviour {
 
 	public AudioClip breakSound;
 
+	public GameObject UI_Cage_open;
+	public GameObject UI_Cage_close;
+
 	void Start(){
 		GameManager.Record_help_Max += 1;
 	}
 
 	void OnTriggerEnter(Collider player) {
 		if (player.CompareTag ("player")) {
-			AudioSource.PlayClipAtPoint (breakSound, player.GetComponent<PlayerMove> ().Camera_ingame.transform.position, GameManager.soundVolume);
-			//breakCage.SetActive (true);
-			//standCage.SetActive (false);
-			//GameManager.Record_help += 1;
+			if(player.GetComponent<PlayerMove>().bounce == Bouncy.Down){
+				AudioSource.PlayClipAtPoint (breakSound, player.GetComponent<PlayerMove> ().Camera_ingame.transform.position, GameManager.soundVolume);
+				breakCage.SetActive (true);
+				standCage.SetActive (false);
+				GameManager.Record_help += 1;
+				UI_Cage_close.SetActive (false);
+				UI_Cage_open.SetActive (true);
+			}
 		}
 	}
 
-	void OnTriggerExit(Collider player) {
-		if (player.CompareTag ("player")) {
-			breakCage.SetActive (true);
-			standCage.SetActive (false);
-			GameManager.Record_help += 1;
-		}
-	}
+
 }
--- Assets/___Scripts/---Ingame/objs/08Cages/UI_Cage.cs
+++ Assets/___Scripts/---Ingame/objs/08Cages/UI_Cage.cs
@@ -4,13 +4,28 @@ using System.Collections;
 public class UI_Cage : MonoBehaviour {
 
 	public GameObject ingameCamera;
-
+	public GameObject model;
 
 
 
 	void Start () {
-		ingameCamera = GameObject.Find ("Main Camera");
+		if (Application.loadedLevelName == "TestGame") {
+			ingameCamera = GameObject.Find ("Main Camera");
+			PosSet ();
+		}
+
 
 	}
 
+	void PosSet() {
+		transform.parent = null;
+		transform.parent = ingameCamera.GetComponent<GameCamera> ().UI_Cage_pos [GameManager.UI_help_index].transform;
+		GameManager.UI_help_index++;
+		transform.localPosition = new Vector3 (0, 0, 0);
+		Invoke ("hideoff", 1f);
+	}
+
+	void hideoff() {
+		model.SetActive (true);
+	}
 }
