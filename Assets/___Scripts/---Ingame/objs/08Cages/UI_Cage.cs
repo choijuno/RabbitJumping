5b9230bb1d2f2e247cce3641fde41adb1113782d
junochoi_PC <hacke11233@gmail.com>
2016-10-14 16:37:29 +0900

    no message

--- Assets/___Scripts/---Ingame/objs/08Cages/UI_Cage.cs
+++ Assets/___Scripts/---Ingame/objs/08Cages/UI_Cage.cs
@@ -3,10 +3,14 @@ using System.Collections;
 
 public class UI_Cage : MonoBehaviour {
 
+	public GameObject ingameCamera;
+
+
 
 
 	void Start () {
-		
+		ingameCamera = GameObject.Find ("Main Camera");
+
 	}
 
 }
