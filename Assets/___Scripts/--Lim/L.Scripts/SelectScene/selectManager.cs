e870ee46d64530be3fb1a98842da91e95d4f924f
LimSihwan <tlghks1009@naver.com>
2016-10-18 13:16:39 +0900

    no message

--- Assets/___Scripts/--Lim/L.Scripts/SelectScene/selectManager.cs
+++ Assets/___Scripts/--Lim/L.Scripts/SelectScene/selectManager.cs
@@ -115,6 +115,7 @@ public partial class selectManager : MonoBehaviour {
     Animator animator;
     void Start ()
     {
+        //ES2.Save(99, "stageIndexCount");
        /* 
         for(int i = 0; i< 100; i++)
         {
